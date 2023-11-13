6c0291f5c09f3bd1b5fd30559ec28fd8b5e2fd33
Akash Deep <akashdeep@Akashs-MacBook-Air.local>
2023-11-13 17:14:44 +0530

    Added chat functionality

--- src/server/socketio/handler.js
+++ src/server/socketio/handler.js
@@ -5,27 +5,23 @@ export async function IOHandler(io){
         
         socket.on('join', (name) => {
             socket.username = name;
-            socket.broadcast.emit('userJoined', name);
+            console.log(`${name} joined the chat`);
+        // Broadcast a message to all clients about the new user joining
+            io.emit('message', { username: name, message: `${name} joined the chat` });
         });
 
-
+        socket.on('message', (data) => {
+            console.log(`Message from ${data.username}: ${data.message}`);
+            // Broadcast the message to all clients
+            io.emit('message', { username: data.username, message: data.message });
+          });
         
-        socket.on("send_message" , (data) => {
-            socket.broadcast.emit("receive_message", {...data, sender : socket.data})
-        })
-
-        socket.on('chatMessage', (message) => {
-            io.emit('chatMessage', {
-              user: socket.username,
-              message: message,
-              time: new Date().toLocaleTimeString(),
-            });
-         });
-         
-
-        socket.on("disconnect", () => {
-            console.log(`Client disconnected: ${socket.id}`)
-        })
+          // Handle user disconnect
+          socket.on('disconnect', () => {
+            console.log('A user disconnected');
+            // Broadcast a message to all clients about the user leaving
+            io.emit('message', { username: 'System', message: 'A user left the chat' });
+          });
    
     })
 }
